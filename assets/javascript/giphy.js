c7830b439a86dc9224be9f68f35dab3006706bf3
Carolyn Smith <casmith205@gmail.com>
2018-04-16 18:44:39 -0500

    changed formatting

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -13,59 +13,59 @@
 // (x) Then make a function call that takes each topic in the array remakes the buttons on the page.
 
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-//SETTING GLOBAL VARIABLES
-    var topics = ["zebra", "monkey", "giraffe", "bear"];
-    var apiKey = "gBMwoy1kuUu2WyiV4tlNZ0Lr9zXGz6Hz"
-    
+  //SETTING GLOBAL VARIABLES
+  var topics = ["zebra", "monkey", "giraffe", "bear"];
+  var apiKey = "gBMwoy1kuUu2WyiV4tlNZ0Lr9zXGz6Hz"
 
-// ON-PAGE EVENTS
+
+  // ON-PAGE EVENTS
   // On the click of the search animal button...
-    $("#add-animal").on("click", function(){
-      event.preventDefault();
-      // This line will grab the text from the input box
-      var animal = $("#search-input").val().trim();
-      topics.push(animal);
-      console.log(topics);
-      renderButtons();
-    });
+  $("#add-animal").on("click", function () {
+    event.preventDefault();
+    // This line will grab the text from the input box
+    var animal = $("#search-input").val().trim();
+    topics.push(animal);
+    console.log(topics);
+    renderButtons();
+  });
 
-// On-click of the gifs 
-    $("body").on("click", ".gif", function() {
-      // Getting the data state of the gif on click
-      var state = $(this).attr("data-state");
-      console.log(state);
-      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-      // Then, set the image's data-state to animate
-      // Else set src to the data-still value
-      if (state === "still") {
-        console.log(this);
-        $(this).attr("data-state", "animate");
-        $(this).attr("src", $(this).attr("gif"))
-      } else {
-        $(this).attr("data-state", "still");
-        $(this).attr("src", $(this).attr("still"))
-      }
-    });
+  // On-click of the gifs 
+  $("body").on("click", ".gif", function () {
+    // Getting the data state of the gif on click
+    var state = $(this).attr("data-state");
+    console.log(state);
+    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+    // Then, set the image's data-state to animate
+    // Else set src to the data-still value
+    if (state === "still") {
+      console.log(this);
+      $(this).attr("data-state", "animate");
+      $(this).attr("src", $(this).attr("gif"))
+    } else {
+      $(this).attr("data-state", "still");
+      $(this).attr("src", $(this).attr("still"))
+    }
+  });
 
-// On-click of the favorite button
-$("body").on("click", ".fav", function() {
+  // On-click of the favorite button
+  $("body").on("click", ".fav", function () {
     console.log(this);
     // How do I move the whole image to favorites
     $(this)
       .closest("div")
       .appendTo($(".favorites"));
     $(this).fadeOut();
-});
+  });
 
-    
-// DEFINING FUNCTIONS
+
+  // DEFINING FUNCTIONS
   // Function for displaying animal buttons
   function renderButtons() {
-  // Deletes the animals prior to adding new animals
-  $(".buttons").empty();
-  // loops through the array and adds the button and appends to the button section
+    // Deletes the animals prior to adding new animals
+    $(".buttons").empty();
+    // loops through the array and adds the button and appends to the button section
     for (var i = 0; i < topics.length; i++) {
       var btn = $("<button>");
       btn.addClass("animal");
@@ -76,17 +76,17 @@ $("body").on("click", ".fav", function() {
   };
 
   // Function for displaying gifs
-  function displayGif (){
+  function displayGif() {
     var animal = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q="+animal+"&limit=10&api_key=" + apiKey;
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&limit=10&api_key=" + apiKey;
 
     $.ajax({
       url: queryURL,
       method: "GET"
-    }).then(function(response) {
-    console.log(response);
-    // Creating a for loop to run through the images in the array
-      for(i=0; i<response.data.length; i++) {
+    }).then(function (response) {
+      console.log(response);
+      // Creating a for loop to run through the images in the array
+      for (i = 0; i < response.data.length; i++) {
         // Creating and storing a div tag
         var animalDiv = $("<div class='animalDiv'>");
         // Creating a paragraph tag with the gif rating
